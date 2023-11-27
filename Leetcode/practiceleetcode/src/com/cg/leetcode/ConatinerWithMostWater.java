631cca9973aa05d3dbb6cfe854d50e13331892d6
BijayKumar1 <pradhanbijay29177@gmail.com>
2023-11-27 14:42:21 +0530

    some other program added to github

---
+++ Leetcode/practiceleetcode/src/com/cg/leetcode/ConatinerWithMostWater.java
@@ -0,0 +1,22 @@
+package com.cg.leetcode;
+
+class ConatinerWithMostWater
+{
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ Leetcode/practiceleetcode/src/module-info.java
@@ -0,0 +1,2 @@
+module practiceleetcode {
+}
