28084f3afc6e3af9b541998aa35f2d64553d7db7
BIJPRADH803 <102797079+BIJPRADH803@users.noreply.github.com>
2023-11-27 07:48:14 +0530

    Add files via upload

---
+++ NOVEMBER0 021123/Leetcode/practiceleetcode/src/com/cg/leetcode/date.java
@@ -0,0 +1,22 @@
+package com.cg.leetcode;
+
+import java.time.LocalDate;
+
+public class date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		LocalDate date=LocalDate.now();
+		LocalDate yesterday=date.minusDays(1);
+		LocalDate tomorrow=yesterday.plusDays(2);
+		
+		LocalDate  tmrw=date.plusDays(1);
+		System.out.println("today date is: " +date);
+		System.out.println("yesterday  is: " +yesterday);
+		System.out.println("tomorrow date is: " +tomorrow);
+		System.out.println("tomorrow  date is: " +tmrw);
+		
+	}
+}
